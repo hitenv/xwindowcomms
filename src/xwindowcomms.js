a5dbcb5a03b3ec980625d488f9441e637bc00138
Hiten Vaghela <hiten.vaghela@gmail.com>
2015-11-30 17:49:20 +0000

    Refactored lib to allow for instantiation of parent and popup independently to entire lib

--- src/xwindowcomms.js
+++ src/xwindowcomms.js
@@ -4,113 +4,117 @@
 /*jslint browser: true */
 /*global console: false */
 /* exported xWindowComms */
-var xWindowComms = function() {
-    var xWindowComms = {
-        parent: {},
-        popup: {}
-    };
-
-    var id = Math.random();
-
-    xWindowComms.parent.popup = null;
-    xWindowComms.handshake = null;
-
-    xWindowComms.parent.connected = false;
-    xWindowComms.buffer = [];
-
-    xWindowComms.parent.initiate = function (url, name, options) {
-        xWindowComms.parent.popup = window.open(url, name, options);
-
-        var payload = {
-            action: 'initiate',
-            status: false
-        };
-
-        xWindowComms.handshake = setInterval(
-            function () {
-                xWindowComms.parent.sendMessage(payload);
-            }, 1000);
-    };
 
 
-    xWindowComms.parent.sendMessage = function (payload) {
-        payload.id = id;
+window.xWindowComms = window.xWindowComms || (function() {
+        var parent = function(){
+            this.popup = null;
+            this.handshake = null;
+            this.connected = false;
+            this.buffer = [];
+            this.id = Math.random();
 
-        if (xWindowComms.parent.connected || payload.action === 'initiate') {
-            xWindowComms.parent.popup.postMessage(payload, '*');
-        } else {
-            xWindowComms.buffer.push(payload);
-        }
-    };
+            this.initiate = function(url, name, options){
+                this.popup = window.open(url, name, options);
 
-    xWindowComms.parent.receiver = function (callback, debug) {
-        var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-        var eventer = window[eventMethod];
-        var messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+                var payload = {
+                    action: 'initiate',
+                    status: false
+                };
 
-        eventer(messageEvent, function (e) {
-            if (debug === true) {
-                console.log(e);
-            }
+                this.handshake = setInterval(
+                    function () {
+                        this.sendMessage(payload);
+                    }.bind(this), 1000);
 
-            if (typeof event.data === 'object') {
-                if (event.data.id === id){
-                    if (event.data.action === 'initiate') {
-                        if (event.data.status === true) {
-                            clearInterval(xWindowComms.handshake);
-                            xWindowComms.parent.connected = true;
-                            if (xWindowComms.buffer.length > 0) {
-                                for (var i = 0; i < xWindowComms.buffer.length; i++) {
-                                    xWindowComms.parent.sendMessage(xWindowComms.buffer[i]);
+            };
+
+            this.sendMessage = function (payload) {
+                payload.id = this.id;
+
+                if (this.connected || payload.action === 'initiate') {
+                    this.popup.postMessage(payload, '*');
+                } else {
+                    this.buffer.push(payload);
+                }
+            };
+
+            this.receiver = function (callback, debug) {
+                var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+                var eventer = window[eventMethod];
+                var messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+
+                eventer(messageEvent, function (e) {
+                    if (debug === true) {
+                        console.log(e);
+                    }
+
+                    if (typeof event.data === 'object') {
+                        if (event.data.id === this.id) {
+                            if (event.data.action === 'initiate') {
+                                if (event.data.status === true) {
+                                    clearInterval(this.handshake);
+                                    this.connected = true;
+                                    if (this.buffer.length > 0) {
+                                        for (var i = 0; i < this.buffer.length; i++) {
+                                            this.sendMessage(this.buffer[i]);
+                                        }
+
+                                        this.buffer = [];
+                                    }
                                 }
-
-                                xWindowComms.buffer = [];
                             }
+
+                            callback(e);
+                        }
+                    }
+                }.bind(this));
+            };
+        };
+        var popup = function(){
+            this.parent = null;
+            this.id = null;
+
+            this.initiate = function (callback, debug) {
+                var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+                var eventer = window[eventMethod];
+                var messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+
+                eventer(messageEvent, function (e) {
+                    if (debug === true) {
+                        console.log(e);
+                    }
+
+                    if (typeof event.data === 'object') {
+                        if (event.data.action === 'initiate') {
+                            var payload = {
+                                action: 'initiate',
+                                status: true
+                            };
+
+                            this.id = event.data.id;
+
+                            this.parent = event.source;
+
+                            this.sendMessage(payload);
                         }
                     }
 
                     callback(e);
-                }
-            }
-        });
-    };
+                }.bind(this));
+            };
 
-    xWindowComms.popup.parent = null;
-    xWindowComms.popup.id = null;
+            this.sendMessage = function(payload){
+                payload.id = this.id;
+                this.parent.postMessage(payload, '*');
+            };
 
-    xWindowComms.popup.initiate = function (callback, debug) {
-        var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-        var eventer = window[eventMethod];
-        var messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+        };
 
-        eventer(messageEvent, function (e) {
-            if (debug === true) {
-                console.log(e);
-            }
+        var xWindowComms = {
+            parent : parent,
+            popup: popup
+        };
 
-            if (typeof event.data === 'object') {
-                if (event.data.action === 'initiate') {
-                    var payload = {
-                        action: 'initiate',
-                        status: true
-                    };
-
-                    xWindowComms.popup.id = event.data.id;
-
-                    xWindowComms.popup.parent = event.source;
-
-                    xWindowComms.popup.sendMessage(payload);
-                }
-            }
-
-            callback(e);
-        });
-    };
-
-    xWindowComms.popup.sendMessage = function (payload) {
-        payload.id = xWindowComms.popup.id;
-        xWindowComms.popup.parent.postMessage(payload, '*');
-    };
-
-    return xWindowComms;
-};
+        return xWindowComms;
+    }());
